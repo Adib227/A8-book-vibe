817ca7a7c0a5c99a1a7a0d563c63a468c2f4176d
Iftekhar Aziz Adib <iftekharadib227@gmail.com>
2024-03-29 23:39:01 +0600

    Solved Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,42 +3,36 @@ import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <>
-      < div className="navbar bg-base-100 lg:p-8 fixed z-10 flex grid-cols-3" >
+      <div className="navbar bg-base-100 fixed z-10 p-12">
         <div className="navbar-start">
-          <div className="btn btn-ghost text-base lg:text-xl font-bold">
-            <Link to='/'>
-              <h1>Book Vibe</h1>
-            </Link>
-          </div>
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul className="flex lg:ml-[500px]">
-              <NavLink to='/' className="btn btn-ghost text-sm lg:text-lg text-[#23BE0A] border-[#23BE0A]">
-                Home
-              </NavLink>
-              <NavLink to='/listedbook' className="btn btn-ghost text-sm lg:text-lg">
-                Listed Books
-              </NavLink>
-              <NavLink to='/pagetoread' className="btn btn-ghost text-sm lg:text-lg">
-                Pages to Read
-              </NavLink>
-              <NavLink to='/islamicbook' className="btn btn-ghost text-sm lg:text-lg">
-                Islamic Book
-              </NavLink>
-              <NavLink to='/ebook' className="btn btn-ghost text-sm lg:text-lg">
-                E Book
-              </NavLink>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <NavLink to="/" className="text-lg rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762]font-normal">Home</NavLink>
+              <NavLink to="/listedbook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Listed Books</NavLink>
+              <NavLink to="/pagetoread" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Pages to Read</NavLink>
+              <NavLink to="/islamicbook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Islamic Book</NavLink>
+              <NavLink to="/ebook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">E Book</NavLink>
             </ul>
           </div>
+          <a className="text-2xl font-bold">Book Vibe</a>
         </div>
-        <div className="navbar-end gap-2">
-          <a className="btn btn-success text-white">Sign In</a>
-          <a className="btn btn-info text-white">Sign Up</a>
-        </div >
-
-      </div >
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-6">
+            <NavLink to="/" className="text-lg rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] font-normal">Home</NavLink>
+              <NavLink to="/listedbook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Listed Books</NavLink>
+              <NavLink to="/pagetoread" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Pages to Read</NavLink>
+              <NavLink to="/islamicbook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">Islamic Book</NavLink>
+              <NavLink to="/ebook" className=" rounded-lg font-bold text-[#38b762] border-2 px-2 border-[#38b762] text-lg ">E Book</NavLink>
+          </ul>
+        </div>
+        <div className="navbar-end gap-2 lg:gap-4 hidden lg:flex">
+          <a className="btn bg-[#38b762] text-lg font-semibold text-white">Sign In</a>
+          <a className="btn text-lg font-semibold text-white bg-[#5ac6d1]">Sign Up</a>
+        </div>
+      </div>
     </>
 
   );
